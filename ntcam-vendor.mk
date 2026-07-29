89884294399eae561fa4eff9b3a7334a926ca171
Builder <builder@localhost>
2026-07-29 16:12:22 +0000

    ntcam: Add libnelib.so copy rule to system_ext/lib64

--- ntcam-vendor.mk
+++ ntcam-vendor.mk
@@ -28,6 +28,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(NTCAM_PATH)/proprietary/system_ext/etc/permissions/privapp-permissions-NothingExperience.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-NothingExperience.xml \
+    $(NTCAM_PATH)/proprietary/system_ext/lib64/libnelib.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libnelib.so \
     $(NTCAM_PATH)/proprietary/vendor/etc/camera/NdotFont/Ndot-55.otf:$(TARGET_COPY_OUT_VENDOR)/etc/camera/NdotFont/Ndot-55.otf \
     $(NTCAM_PATH)/proprietary/vendor/etc/camera/Robotofont/Roboto-55.ttf:$(TARGET_COPY_OUT_VENDOR)/etc/camera/Robotofont/Roboto-55.ttf \
     $(NTCAM_PATH)/proprietary/vendor/etc/camera/SansCJKFont/NotoSansCJK.ttc:$(TARGET_COPY_OUT_VENDOR)/etc/camera/SansCJKFont/NotoSansCJK.ttc \
